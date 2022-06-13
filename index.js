4bc634438ffee958895a41f6c980dac5946aeb4a
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-06-12 20:19:28 -0500

    arrow move updated gif

--- index.js
+++ index.js
@@ -3,26 +3,48 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
 
-setInterval(function() {
-if(direction === 'west'){
-    x = x - 1
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
 }
-if(direction === 'north'){
-    y = y + 1
-}
-if(direction === 'east'){
-    x = x + 1
-}
-if(direction === 'south'){
-    y = y - 1
-}
-character.style.left = x + 'px'
-character.style.bottom = y + 'px'
-}, 1)
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+
+// let direction = null;
+// let x = 100;
+// let y = 250;
+
+// setInterval(function() {
+// if(direction === 'west'){
+//     x = x - 1
+// }
+// if(direction === 'north'){
+//     y = y + 1
+// }
+// if(direction === 'east'){
+//     x = x + 1
+// }
+// if(direction === 'south'){
+//     y = y - 1
+// }
+// character.style.left = x + 'px'
+// character.style.bottom = y + 'px'
+// }, 1)
 
 //setInterval(moveCharacter, 1)                                    
 
